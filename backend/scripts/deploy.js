672cf251d6a68d3dc61dbeb2d763a8998d66775a
b0nesss <bighnesh147@gmail.com>
2023-07-13 11:18:28 +0530

    backend structure

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,16 @@
+
+const hardhat = require(`hardhat`);
+
+
+async function main() {
+
+}
+
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
